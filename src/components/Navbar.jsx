da7875cc5e533815e0942673467425d7b9ceb0ec
Patrick Njiru <patricknjiru93@gmail.com>
2024-07-26 00:26:58 +0300

    Add more functionaliy to the navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,44 @@
 import { useEffect, useState } from "react"
-import { FaCartArrowDown } from "react-icons/fa"
+import { FaArrowUp, FaCartArrowDown } from "react-icons/fa"
 
 const Navbar = () => {
-	const [scrollHeight, setScrollHeight] = useState(0)
+	const [scrollPosition, setScrollPosition] = useState(0)
+	const [screenWidth, setScreenWidth] = useState(window.innerWidth)
 	const [openMenu, setOpenMenu] = useState(false)
-	const [cartItems, setCartItems] = useState(0)
 
-	useEffect(() => setScrollHeight(window.scrollY), [])
+	useEffect(() => {
+		const handleScroll = () => setScrollPosition(window.scrollY)
+
+		const handleResize = () => setScreenWidth(window.innerWidth)
+
+		window.addEventListener("scroll", handleScroll)
+		window.addEventListener("resize", handleResize)
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll)
+			window.removeEventListener("resize", handleResize)
+		}
+	}, [])
 
 	return (
 		<header
 			className={`
-        flex justify-between items-center w-full py-8 px-10 fixed start-0 top-0 z-10 bg-inherit
-        ${scrollHeight > 50 ? "bg-black" : "bg-transparent"} `}
+        flex justify-between items-center w-full fixed start-0 top-0 z-10
+				transition-colors duration-100 ease-linear
+        ${scrollPosition == 0 ? "bg-transparent pt-3 xl:pt-0" : "bg-black"} `}
 		>
-			<picture>
-				<source media='(min-width: 1024px)' srcSet='https://fakeimg.pl/50x50' />
-				<source media='(max-width: 1023px)' srcSet='https://fakeimg.pl/40x25' />
-				<img src='https://fakeimg.pl/50x50' />
-			</picture>
+			<a href='#'>
+				<img
+					src={`${
+						scrollPosition == 0
+							? screenWidth < 1024
+								? "https://fakeimg.pl/25x25"
+								: "https://fakeimg.pl/80x80"
+							: "https://fakeimg.pl/80x80"
+					}`}
+					alt='logo'
+				/>
+			</a>
 			{/* Desktop */}
 			<nav className='ms-60 w-1/2 gap-3 hidden justify-evenly bg-inherit [&_*]:font-medium lg:flex'>
 				<a href='#'>HOME</a>
@@ -29,18 +49,11 @@ const Navbar = () => {
 			</nav>
 
 			{/* Mobile */}
-			<button
-				className='bg-inherit absolute top-9 end-36 space-y-0.5'
-				onClick={() => setOpenMenu(!openMenu)}
-			>
-				<span id='line1' className='block h-1 w-5 bg-white'></span>
-				<span id='line2' className='block h-1 w-4 bg-white'></span>
-				<span id='line3' className='block h-1 w-6 bg-white'></span>
-			</button>
+
 			<nav
 				className={`
-          transition-all duration-300 ease-out fixed top-0  w-1/2 flex flex-col
-          z-20 gap-6 items-center py-20 rounded-bl-xl bg-teal-500 lg:hidden
+          transition-all duration-300 ease-out fixed top-0 w-1/2 flex flex-col
+          z-20 gap-6 items-center py-20 rounded-l-xl bg-teal-500 lg:hidden
           ${openMenu ? "end-0" : "-end-1/2"}
           `}
 			>
@@ -58,12 +71,28 @@ const Navbar = () => {
 			</nav>
 
 			<div className='flex gap-1 w-fit bg-inherit relative'>
+				<button
+					className='bg-inherit space-y-0.5 me-6 lg:hidden'
+					onClick={() => setOpenMenu(!openMenu)}
+				>
+					<span id='line1' className='block h-1 w-5 bg-white'></span>
+					<span id='line2' className='block h-1 w-4 bg-white'></span>
+					<span id='line3' className='block h-1 w-6 bg-white'></span>
+				</button>
 				<span>$0.00</span>
 				<FaCartArrowDown className='size-6' />
 				<span className='text-sm -block size-4 flex justify-center items-center bg-red-500 rounded-full absolute -end-1 -top-2'>
-					{cartItems}
+					0
 				</span>
 			</div>
+			<a
+				href='#'
+				className={`fixed bottom-10 end-10 ${
+					scrollPosition > window.innerHeight ? "" : "hidden"
+				}`}
+			>
+				<FaArrowUp />
+			</a>
 		</header>
 	)
 }
