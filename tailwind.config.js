3e9d1e7001d720b05549ae3ffcede22bda1aa5fb
Patrick Njiru <patricknjiru93@gmail.com>
2024-07-25 17:14:26 +0300

    Initial commit.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			// colors: {
+			// 	"custom-yellow": "#ddbe10",
+			// 	"custom-dark-blue": "#0a081d",
+			// 	"custom-gray": "rgb(92, 91, 91)",
+			// },
+		},
+	},
+	plugins: [],
+}
