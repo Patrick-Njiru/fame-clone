4487fddfac03639a19ff3d0c60511eb456c5e428
Patrick Njiru <patricknjiru93@gmail.com>
2024-07-25 20:27:03 +0300

    Import necessary components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { About, CTA, Footer, Hero, Navbar, Service, Testimonials } from "./components"
+
 const App = () => {
-	return <div>App</div>
+	return (
+		<main className="px-10 pt-20 overflow-hidden">
+			<Navbar />
+			<Hero />
+			<About />
+			<Service />
+			<Testimonials />
+			<CTA />
+			<Footer />
+		</main>
+	)
 }
 
 export default App
