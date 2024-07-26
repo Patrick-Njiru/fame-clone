e05dbec47d2884070abfcaea9386228327ec4460
Patrick Njiru <patricknjiru93@gmail.com>
2024-07-26 11:09:40 +0300

    Add the contact section (incomplete) and improve the Button component.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,11 @@ import PropTypes from "prop-types"
 
 const Button = ({ children }) => {
 	return (
-		<button className='w-fit uppercase bg-orange-500 text-center py-2 px-4 rounded-sm hover:bg-orange-600'>
+		<button
+			className='
+				w-fit uppercase bg-orange-500 text-center py-3 px-6 rounded-sm
+				transition-all duration-300 ease-linear relative top-0 hover:-top-3 hover:bg-orange-600'
+		>
 			{children}
 		</button>
 	)
--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -1,5 +1,28 @@
+import { Button } from "../components"
+
 const Contact = () => {
-	return <section id='contact'>Contact</section>
+	return (
+		<section
+			id='contact'
+			className='py-40 bg-zinc-800'
+			style={{
+				// backgroundImage: "url(https://via.placeholder.com/1024?text=More+Cars)",
+				// backgroundRepeat: "no-repeat",
+				// backgroundSize: "cover",
+				// backgroundBlendMode: "overlay",
+			}}
+		>
+			<div className='space-y-6 text-center w-4/5 mx-auto'>
+				<h1 className='text-5xl'>Contact Us, ad let&apos;s chat over a cup of coffee</h1>
+				<p className='w-5/6 mx-auto'>
+					We look forward to serving you and ensuring your rental experience is as
+					exceptional as the vehicles we provide. For any enquiries, please contact us
+					through the details provided in the official website.
+				</p>
+				<Button>Rent now</Button>
+			</div>
+		</section>
+	)
 }
 
 export default Contact
