70c3b39511d96811d76e11733e1e7f73a3294647
Patrick Njiru <patricknjiru93@gmail.com>
2024-07-26 18:01:17 +0300

    Make contact page mobile responsive.

--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -10,12 +10,12 @@ const Contact = () => {
 				backgroundImage: `url(${contactBg})`,
 				backgroundRepeat: "no-repeat",
 				backgroundSize: "cover",
-				backgroundBlendMode: 'color-dodge',
-				backgroundPosition: 'center center',
+				backgroundBlendMode: "color-dodge",
+				backgroundPosition: "center center",
 			}}
 		>
-			<div className='space-y-6 text-center w-4/5 mx-auto'>
-				<h1 className='text-5xl'>Contact Us, ad let&apos;s chat over a cup of coffee</h1>
+			<div className='space-y-6 text-center mx-auto md:w-4/5'>
+				<h1 className='text-4xl md:text-6xl'>Contact Us, ad let&apos;s chat over a cup of coffee</h1>
 				<p className='w-5/6 mx-auto'>
 					We look forward to serving you and ensuring your rental experience is as
 					exceptional as the vehicles we provide. For any enquiries, please contact us
