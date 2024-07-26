f92819a7f4cba6cea7f72ec105958b82cd61d884
Patrick Njiru <patricknjiru93@gmail.com>
2024-07-26 17:32:23 +0300

    Make the Services section mobile responsive.

--- src/scenes/Service.jsx
+++ src/scenes/Service.jsx
@@ -2,57 +2,54 @@ import { SectionTitle } from "../components"
 
 const Service = () => {
 	return (
-		<section
-			id='services'
-			className='py-40 bg-orange-500 bg-opacity-30 relative h-[200vh] overflow-hidden'
-		>
+		<section id='services' className='py-40 bg-[#FF7F04] relative overflow-hidden'>
 			<SectionTitle
 				title='The power of our service'
 				description='Why choose fame'
-				otherStyles='text-white [&_h2]:text-black bg-red'
+				otherStyles='text-white [&_h2]:text-black bg-red relative z-10'
 			/>
-			<div className='flex justify-center gap-10 my-40 mt-20'>
-				<article className='w-44 h-52 text-2xl font-black relative border-l-2'>
+			<div className='flex flex-wrap justify-center gap-10 mb-10 mt-20 relative z-10 md:flex-nowrap md:mb-80'>
+				<article className='w-20 md:w-44 h-52 text-2xl font-black relative border-l-2'>
 					<span className='block size-2 bg-white rounded-full absolute -start-[5px] top-0'></span>
 					<span className='block size-5 bg-white rounded-full absolute bottom-0 -start-[10px]'></span>
 					<p className='ms-4'>Well Maintained Cars</p>
 				</article>
-				<article className='w-44 h-44 text-2xl font-black relative border-l-2'>
+				<article className='w-20 md:w-44 h-44 text-2xl font-black relative border-l-2'>
 					<span className='block size-2 bg-white rounded-full absolute -start-[5px] top-0'></span>
 					<span className='block size-5 bg-white rounded-full absolute bottom-0 -start-[10px]'></span>
 					<p className='ms-4'>Professional Staff</p>
 				</article>
-				<article className='w-44 h-36 text-2xl font-black relative border-l-2'>
+				<article className='w-20 md:w-44 h-36 text-2xl font-black relative border-l-2'>
 					<span className='block size-2 bg-white rounded-full absolute -start-[5px] top-0'></span>
 					<span className='block size-5 bg-white rounded-full absolute bottom-0 -start-[10px]'></span>
 					<p className='ms-4'>Affordable Luxury</p>
 				</article>
-				<article className='w-44 h-28 text-2xl font-black relative border-l-2'>
+				<article className='w-20 md:w-44 h-28 text-2xl font-black relative border-l-2'>
 					<span className='block size-2 bg-white rounded-full absolute -start-[5px] top-0'></span>
 					<span className='block size-5 bg-white rounded-full absolute bottom-0 -start-[10px]'></span>
 					<p className='ms-4'>Easy Booking Process</p>
 				</article>
 			</div>
-			<div className='absolute top-0 start-0 w-screen flex justify-center -z-10'>
+			<div className='relative w-full flex justify-center mx-auto -top-[80vh]'>
 				<img
 					src='/src/assets/ferrari-bg.webp'
 					alt='car'
-					className='object-cover size-full  flex'
+					className='absolute -top-10 hidden md:inline'
 				/>
 			</div>
 
-			<div className='flex justify-end me-16 gap-10 z-10 mt-80'>
-				<article className='w-52 h-24 text-2xl font-black relative border-l-2 flex items-end top-16'>
+			<div className='flex flex-wrap justify-center gap-12 z-10 md:mt-28 md:justify-end md:me-16 md:flex-nowrap lg:mt-40'>
+				<article className='w-20 md:w-52 h-24 text-2xl font-black relative border-l-2 flex items-end top-16'>
 					<span className='block size-2 bg-white rounded-full absolute -start-[5px] bottom-0'></span>
 					<span className='block size-5 bg-white rounded-full absolute top-0 -start-[10px]'></span>
 					<p className='ms-4'>No Deposit Required</p>
 				</article>
-				<article className='w-52 h-32 text-2xl font-black relative border-l-2 flex items-end top-8'>
+				<article className='w-20 md:w-52 h-32 text-2xl font-black relative border-l-2 flex items-end top-8'>
 					<span className='block size-2 bg-white rounded-full absolute -start-[5px] bottom-0'></span>
 					<span className='block size-5 bg-white rounded-full absolute top-0 -start-[10px]'></span>
 					<p className='ms-4'>Diverse Fleet of Luxury Cars</p>
 				</article>
-				<article className='w-52 h-40 text-2xl font-black relative border-l-2 flex items-end'>
+				<article className='w-20 md:w-52 h-40 text-2xl font-black relative border-l-2 flex items-end'>
 					<span className='block size-2 bg-white rounded-full absolute -start-[5px] bottom-0'></span>
 					<span className='block size-5 bg-white rounded-full absolute top-0 -start-[10px]'></span>
 					<p className='ms-4'>Exceptional Customer Care</p>
